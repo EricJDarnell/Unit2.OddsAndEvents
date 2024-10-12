719a5ef6a0f12bf2c50dc827fd0a47b2979fc5a8
EricJDarnell <eric.j.darnell@gmail.com>
2024-10-12 10:47:28 -0400

    fixed bug: sort one when number bank is empty

--- index.js
+++ index.js
@@ -4,21 +4,21 @@ const state = {
   Evens: [],
 };
 function showState() {
-    if (dropdown.value === 'ascending') {
-        state.Odds.sort((a,b) => a - b );  //sorts into ascending order
-        state.Evens.sort((a,b) => a - b );
-    } else {
-        const ascendingOdds = state.Odds.sort((a,b) => a - b ); //more involved for descending order
-        state.Odds = [];
-        for (let i = 0; i < ascendingOdds.length; i++) {
-            state.Odds.push(ascendingOdds[ascendingOdds.length - i - 1]);
-        }
-        const ascendingEvens = state.Evens.sort((a,b) => a - b );
-        state.Evens = [];
-        for (let i = 0; i < ascendingEvens.length; i++) {
-            state.Evens.push(ascendingEvens[ascendingEvens.length - i - 1]);
-        }    
+  if (dropdown.value === "ascending") {
+    state.Odds.sort((a, b) => a - b); //sorts into ascending order
+    state.Evens.sort((a, b) => a - b);
+  } else {
+    const ascendingOdds = state.Odds.sort((a, b) => a - b); //more involved for descending order
+    state.Odds = [];
+    for (let i = 0; i < ascendingOdds.length; i++) {
+      state.Odds.push(ascendingOdds[ascendingOdds.length - i - 1]);
     }
+    const ascendingEvens = state.Evens.sort((a, b) => a - b);
+    state.Evens = [];
+    for (let i = 0; i < ascendingEvens.length; i++) {
+      state.Evens.push(ascendingEvens[ascendingEvens.length - i - 1]);
+    }
+  }
   // output element in the section id="numberBank"
   const sectionBank = document.querySelector("#numberBank");
   const bankOutput = sectionBank.querySelector("output");
@@ -46,42 +46,38 @@ function showState() {
   }
 }
 function cleanInput(str) {
-    const regex = /[\D]/g;
-    return str.replace(regex,"");
+  const regex = /[\D]/g;
+  return str.replace(regex, "");
 }
-
 
 const formElem = document.querySelector("form");
 formElem.addEventListener("submit", (evt) => {
   evt.preventDefault();
-   userInput = document.querySelector("#number").value;
-  if (userInput !== ""){
-    const inputArray = userInput.split(","); 
-    for (let i=0; i < inputArray.length; i++){
-        if (cleanInput(inputArray[i]) !== ""){
-            state.numberBank.push(cleanInput(inputArray[i]) * 1);
-        }
+  userInput = document.querySelector("#number").value;
+  if (userInput !== "") {
+    const inputArray = userInput.split(",");
+    for (let i = 0; i < inputArray.length; i++) {
+      if (cleanInput(inputArray[i]) !== "") {
+        state.numberBank.push(cleanInput(inputArray[i]) * 1);
+      }
     }
-        
-      showState();
+
+    showState();
   } else {
-    console.log("do nothing");//don't need to leave this
+    console.log("do nothing"); //don't need to leave this
   }
   /** Adding changes here for extension*/
-
-
-
 });
 const randomButton = document.querySelector("#randomButton"); //extension!!
 randomButton.addEventListener("click", (evt) => {
-    evt.preventDefault()
-    const randomNumber = Math.floor(Math.random() * 100);
-    if (Math.floor(Math.random()*20) < 7){
-        state.numberBank.push(-1*randomNumber);
-    } else{
-        state.numberBank.push(randomNumber);
-    }
-    showState();
+  evt.preventDefault();
+  const randomNumber = Math.floor(Math.random() * 100);
+  if (Math.floor(Math.random() * 20) < 7) {
+    state.numberBank.push(-1 * randomNumber);
+  } else {
+    state.numberBank.push(randomNumber);
+  }
+  showState();
 });
 
 const sortOne = document.querySelector("#sortOne");
@@ -89,54 +85,55 @@ sortOne.addEventListener("click", (evt) => {
   evt.preventDefault(); //keeps the page from refreshing when you click them buttons
   //sort into the odd and even arrays easy with if/then
   const uno = state.numberBank[0];
-  if (uno % 2 === 0) {
-    state.Evens.push(uno);
-  } else {
-    state.Odds.push(uno);
+  if (uno !== undefined) {
+    if (uno % 2 === 0) {
+      state.Evens.push(uno);
+    } else {
+      state.Odds.push(uno);
+    }
   }
   state.numberBank.shift();
   showState();
-
 });
 
 const sortAll = document.querySelector("#sortAll");
 sortAll.addEventListener("click", (evt) => {
   evt.preventDefault();
-  while (state.numberBank.length !== 0) { //started with a for loop, and then took a bit of time to figure out why that wasn't working.
+  while (state.numberBank.length !== 0) {
+    //started with a for loop, and then took a bit of time to figure out why that wasn't working.
     const uno = state.numberBank[0];
     if (uno % 2 === 0) {
       state.Evens.push(uno);
     } else {
       state.Odds.push(uno);
     }
-    state.numberBank.shift()
+    state.numberBank.shift();
   }
   showState();
 });
 
-const numSort = document.querySelector('#numSort');
-const numSortSpan = document.querySelector('#numSortSpan');
-numSort.addEventListener('input', () => {
-    numSortSpan.innerText = numSort.value;
-} );
+const numSort = document.querySelector("#numSort");
+const numSortSpan = document.querySelector("#numSortSpan");
+numSort.addEventListener("input", () => {
+  numSortSpan.innerText = numSort.value;
+});
 
-const numSortButton = document.querySelector('#numSortButton');
-numSortButton.addEventListener('click', (evt) => {
-    evt.preventDefault();
-    for (let i = 0; i < numSort.value; i++ ) {
-        const uno = state.numberBank[0];
-        if (uno === undefined) {
-            console.log("do nothing"); // I'm not sure how to do nothing
-        }
-        else if (uno % 2 === 0) {
-          state.Evens.push(uno);
-        } else {
-          state.Odds.push(uno);
-        }
-        state.numberBank.shift()
+const numSortButton = document.querySelector("#numSortButton");
+numSortButton.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  for (let i = 0; i < numSort.value; i++) {
+    const uno = state.numberBank[0];
+    if (uno !== undefined) {
+      if (uno % 2 === 0) {
+        state.Evens.push(uno);
+      } else {
+        state.Odds.push(uno);
       }
-      showState();
+      state.numberBank.shift();
+    }
+  }
+  showState();
 });
 
 const dropdown = document.querySelector("#dropdown");
-dropdown.addEventListener('input', showState); 
+dropdown.addEventListener("input", showState);
